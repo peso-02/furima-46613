7205bdd54c80fb1ac5ea84748aa4c719bc3cb832
peso-02 <xwpswgzv7j@privaterelay.appleid.com>
2026-02-14 14:50:08 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/,
+                                 message: 'は6文字以上の半角英数字で、英字と数字の両方を含めてください' }, allow_blank: true
+  validates :nickname, presence: true
+  validates :first_name, :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :first_name_kana, :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
+  validates :birth_date, presence: true
 end
