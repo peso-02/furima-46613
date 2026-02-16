aa98a9d34e064d21577593e9d1a91781adf826cc
peso-02 <xwpswgzv7j@privaterelay.appleid.com>
2026-02-16 19:05:51 +0900

    ユーザーモデルの単体テストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/,
-                                 message: 'は6文字以上の半角英数字で、英字と数字の両方を含めてください' }, allow_blank: true
+  validates :password,
+            format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: 'is invalid. Include both letters and numbers' },
+            allow_blank: true
   validates :nickname, presence: true
-  validates :first_name, :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-  validates :first_name_kana, :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
+  validates :first_name, :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/,
+                                                               message: 'is invalid. Input full-width characters' }
+  validates :first_name_kana, :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/,
+                                                                         message: 'is invalid. Input full-width katakana characters' }
   validates :birth_date, presence: true
 end
