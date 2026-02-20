eab759ae3508cf9ef73a3a8d7b09d537fbb8e87c
peso-02 <xwpswgzv7j@privaterelay.appleid.com>
2026-02-20 14:34:24 +0900

    Itemsコントローラ作成・基本アクション実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,40 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity # 422
+    end
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :name,
+      :description,
+      :price,
+      :category_id,
+      :condition_id,
+      :shipping_fee_status_id,
+      :prefecture_id,
+      :shipping_day_id,
+      :image
+    ).merge(user_id: current_user.id)
   end
 end
