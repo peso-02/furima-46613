0f691b2125a2f9e88ed937957d870b358b74924a
peso-02 <xwpswgzv7j@privaterelay.appleid.com>
2026-02-20 19:33:08 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,14 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   # associations
   belongs_to :user
   has_one :order
 
   has_one_attached :image
+
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :shipping_day
 end
