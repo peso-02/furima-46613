d9d6fc016fb6e708aa2678040913ea107b14647f
peso-02 <xwpswgzv7j@privaterelay.appleid.com>
2026-03-02 04:33:41 +0900

    ordersコントローラーにアクセス制限を追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,35 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item
+  before_action :redirect_invalid_access
+
   def index
+    @order_address = OrderAddress.new
   end
 
   def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def redirect_invalid_access
+    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
+  end
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city,
+                                          :house_number, :building_name, :phone_number, :token)
+          .merge(user_id: current_user.id, item_id: @item.id)
   end
 end
