f8d016324646c4bdcbed39b85c437a2c6229da50
peso-02 <xwpswgzv7j@privaterelay.appleid.com>
2026-03-03 17:09:06 +0900

    決済機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,6 +10,7 @@ class OrdersController < ApplicationController
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -25,6 +26,15 @@ class OrdersController < ApplicationController
 
   def redirect_invalid_access
     redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 
   def order_params
